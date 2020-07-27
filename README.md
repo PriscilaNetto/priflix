ba0c4ca82c3253fd38551f9030ab9cd27167e152
Priscila Netto <priscila.nnetto@hotmail.com>
2020-07-27 16:39:28 -0300

    configuracao no app o footer, banner e o carousel

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # priflix
+
+![priflix logo](./src/assets/img/Logo.png)
+
 Projeto desenvolvido durante as aulas de imersão de React pela Alura
+
+## Como instalar
+
+Baixe o projeto e no terminal digite os seguintes comandos:
+
+npm install
+npm start
--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import React from "react";
 import Menu from "./components/Menu";
+import dadosIniciais from "./data/dados_iniciais.json";
+import BannerMain from "./components/BannerMain";
+import Carousel from "./components/Carousel";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
       <Menu />
+
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={
+          "O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"
+        }
+      />
+
+      <Carousel ignoreFirstVideo category={dadosIniciais.categorias[0]} />
+
+      <Carousel ignoreFirstVideo category={dadosIniciais.categorias[1]} />
+
+      <Carousel ignoreFirstVideo category={dadosIniciais.categorias[2]} />
+
+      <Carousel ignoreFirstVideo category={dadosIniciais.categorias[3]} />
+
+      <Carousel ignoreFirstVideo category={dadosIniciais.categorias[4]} />
+
+      <Carousel ignoreFirstVideo category={dadosIniciais.categorias[5]} />
+
+      <Footer />
     </div>
   );
 }
