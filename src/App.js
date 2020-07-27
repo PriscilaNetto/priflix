dd3797aa5efa78943a37247d8faad01f592b15b5
Priscila Netto <priscila.nnetto@hotmail.com>
2020-07-27 20:30:13 -0300

    alteracao dos videos no carousel e categorias

--- src/App.js
+++ src/App.js
@@ -20,15 +20,15 @@ function App() {
 
       <Carousel ignoreFirstVideo category={dadosIniciais.categorias[0]} />
 
-      <Carousel ignoreFirstVideo category={dadosIniciais.categorias[1]} />
+      <Carousel category={dadosIniciais.categorias[1]} />
 
-      <Carousel ignoreFirstVideo category={dadosIniciais.categorias[2]} />
+      <Carousel category={dadosIniciais.categorias[2]} />
 
-      <Carousel ignoreFirstVideo category={dadosIniciais.categorias[3]} />
+      <Carousel category={dadosIniciais.categorias[3]} />
 
-      <Carousel ignoreFirstVideo category={dadosIniciais.categorias[4]} />
+      <Carousel category={dadosIniciais.categorias[4]} />
 
-      <Carousel ignoreFirstVideo category={dadosIniciais.categorias[5]} />
+      <Carousel category={dadosIniciais.categorias[5]} />
 
       <Footer />
     </div>
