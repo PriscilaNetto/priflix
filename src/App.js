bc06893bf5250462ed0dfdc0ec2e7f8779cd77c2
Priscila Netto <priscila.nnetto@hotmail.com>
2020-07-28 11:43:20 -0300

    alteracao do favicon e bannerMain

--- src/App.js
+++ src/App.js
@@ -13,9 +13,7 @@ function App() {
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={
-          "O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"
-        }
+        videoDescription={""}
       />
 
       <Carousel ignoreFirstVideo category={dadosIniciais.categorias[0]} />
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import { FooterBase } from './styles';
+import React from "react";
+import { FooterBase } from "./styles";
+import Logo from "../../assets/img/Logo.png";
 
 function Footer() {
   return (
     <FooterBase>
       <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+        <img className="Logo" src={Logo} alt="priflix logo" />
       </a>
       <p>
-        Orgulhosamente criado durante a
-        {' '}
-        <a href="https://www.alura.com.br/">
-          Imersão React da Alura
-        </a>
+        Orgulhosamente criado por Priscila Netto durante a{" "}
+        <a href="https://www.alura.com.br/">Imersão React da Alura</a>{" "}
+      </p>
+
+      <p>
+        <a href="https://br.linkedin.com/in/priscila-netto">Linkedin |</a>{" "}
+        <a href="https://github.com/PriscilaNetto">GitHub</a>
       </p>
     </FooterBase>
   );
